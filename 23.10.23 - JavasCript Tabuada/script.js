203e6b10754627daf92e9963217a688e8b9255f6
unknown <gabriel.pereira503@etec.sp.gov.br>
2023-10-23 20:29:36 -0300

    Adicionada pasta aula Tabuada em JavaScript

---
+++ 23.10.23 - JavasCript Tabuada/script.js
@@ -0,0 +1,24 @@
+//let btn_tab=document.querySelector('#btn_tabuada')
+//btn_tab.addEventListener
+
+
+
+
+document.querySelector('.btn_tabuada').addEventListener('click', ()=>{
+
+    let numero = Number(
+        document.querySelector('#numero').value
+    )
+
+    for(let i=1; i<=10; i++){
+
+        let res=numero*i
+        let p=document.createElement('p') //criando um parágrafo com a tag 'p' no HTML
+
+        p.textContent=numero + "x" + i + "=" + res
+        document.querySelector('#resultado').append(p) //inserindo a tag 'p'
+
+        
+    }
+
+})
